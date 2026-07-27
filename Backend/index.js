a1a3bcf68884606f07aaf9f6f559c23f822c27c8
parth8454 <parth8454prajapati@gmail.com>
2026-07-27 14:00:00 +0530

    changed variables

--- Backend/index.js
+++ Backend/index.js
@@ -12,9 +12,9 @@ require('./models/db');
 const port = 9090 || process.env.PORT;
 
 app.use(bodyparser.json());
-app.use(express.json());
+app.use(express.json());    
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:`${process.env.FRONTEND_URL}`,
     methods:['GET','POST','PUT','DELETE'],
     credentials:true
 }));
